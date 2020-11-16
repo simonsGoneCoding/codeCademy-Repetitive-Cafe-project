2770779cffaa9b67a139c0a0b58a66d1a7cd7b41
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-16 09:03:47 +0000

    Add foreach loop iterating through drink and prices.

--- index.php
+++ index.php
@@ -29,7 +29,9 @@
     <h1>Welcome to the Repetitive Cafe</h1>
     <h3>Drinks!</h3>
       <ul>
- 
+        <?php foreach($drinks as $drink => $price): ?>
+          <li><?= "$drink: £$price" ?></li>
+        <?php endforeach; ?>
       </ul>
     
     <h3>Pastries! ($2 each)</h3>
