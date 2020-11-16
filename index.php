0bb2b0a47765601b1e1e60528516ae7a41bd41eb
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-11-16 08:58:13 +0000

    Prepare HTML structure and PHP arrays with products and prices.

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+  $drinks = [
+  "Latte" => 3.99,
+  "Coffee" => 2.00,
+  "Tea" => 2.50,
+  "Mocha" => 4.50
+  ];
+
+  $pastries = [
+  "Croissant",
+  "Muffin",
+  "Slice of Pie",
+  "Slice of Cake",
+  "Cupcake",
+  "Brownie"
+  ];
+?>
+
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  
+  <body>
+    
+    <h1>Welcome to the Repetitive Cafe</h1>
+    <h3>Drinks!</h3>
+      <ul>
+ 
+      </ul>
+    
+    <h3>Pastries! ($2 each)</h3>
+      <ul>
+
+      </ul>
+  
+  </body>
+</html>
